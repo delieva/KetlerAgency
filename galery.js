fda22a03cbb965b37649e7772ca406c8ddd20920
delieva <delievaalexandra@gmail.com>
2019-04-20 19:55:07 +0300

    Filter and norm pagonation

---
+++ galery.js
@@ -0,0 +1,243 @@
+(function(){
+	
+	let place = 'city', meaning = 'Kyiv', maxCost, minCost;
+	
+	function filter_by_cost(value){
+		if(!maxCost || !minCost){
+			if(!maxCost && !minCost){
+				return true;
+			}
+			else if(!maxCost){
+				return value.cost >= minCost;
+			}
+			else{
+				return value.cost <= maxCost;
+			}
+		}
+		else{
+			return value.cost >= minCost && value.cost <= maxCost;
+		}
+	}
+	
+	
+	function filter_by_district(value){
+		// if(place === '' && meaning === ''){
+		// 	return true;
+		// }
+		// else{
+			return value.district === document.getElementsByClassName('filter_district')[0].value;
+		// }
+	}
+	
+	function filter_by_city(value){
+		// if(place === '' && meaning === ''){
+		// 	return true;
+		// }
+		// else{
+		// 	for(let key in value){
+		// 		if(key === place){
+					return value.city === document.getElementsByClassName('filter_city')[0].value;
+		// 		}
+		// 	}
+		// }
+	}
+	
+	function filter(someData, city, district) {
+		console.log(city);
+		if(city){
+			let sdata = someData.filter(filter_by_city);
+			if(district){
+				sdata = sdata.filter(filter_by_district);
+				return sdata
+			}
+			return sdata;
+		}
+		else if(!city && district){
+			alert('Please, choose the city before choosing district')
+			return someData;
+		}
+		else{
+			return someData;
+		}
+	}
+	
+	let config = {
+			max_results: 1,
+			max_per_page: 6,
+			page: 1
+		},
+		nOfPages = Math.ceil( config.max_results / config.max_per_page );
+
+
+	function changeConfigs(someData){
+		config = {
+				max_results: someData.length,
+				max_per_page: 6,
+				page: 1
+			},
+			nOfPages = Math.ceil( config.max_results / config.max_per_page );
+	}
+
+
+	//Function for initialisation the number of the page and pages controller
+	function init(someData){
+		document.getElementsByClassName("page_nav")[0].innerHTML = build_nav(someData);
+		document.getElementsByClassName("page_nav")[1].innerHTML = build_nav(someData);
+		
+		document.getElementsByClassName("next")[0].onclick = function() {
+			pager("next", someData);
+			return false;
+		};
+		document.getElementsByClassName("next")[1].onclick = function() {
+			pager("next", someData);
+			return false;
+		};
+		document.getElementsByClassName("previous")[0].onclick = function() {
+			pager("previous", someData);
+			return false;
+		};
+		document.getElementsByClassName("previous")[1].onclick = function() {
+			pager("previous", someData);
+			return false;
+		};
+		document.getElementsByClassName("page_nav")[0].onclick = function(e) {
+			let page = e.target.getAttribute("data-page");
+			if(page){
+				pager("goto", someData, page);
+			}
+			return false;
+		};
+		document.getElementsByClassName("page_nav")[1].onclick = function(e) {
+			let page = e.target.getAttribute("data-page");
+			if(page){
+				pager("goto", someData, page);
+			}
+			return false;
+		};
+		
+		update_page(someData);
+	}
+
+	//Work with next/prev/goto
+	function pager(action, someData, page) {
+		//const nOfPages = someData.length;
+		console.log(config.page + ' ' + nOfPages)
+		document.getElementsByClassName('pagination_page')[config.page-1].style.backgroundColor = "#ffffff";
+		document.getElementsByClassName('pagination_page')[config.page-1 + nOfPages].style.backgroundColor = "#ffffff";
+		switch (action) {
+			case "next":
+				if( (config.page + 1) < nOfPages){
+					++config.page;
+				}
+				break;
+
+			case "previous":
+				if( (config.page - 1) >= 1 ){
+					--config.page;
+				}
+				break;
+
+			case "goto":
+				config.page = page;
+				break;
+
+			default:
+				break;
+		}
+		update_page(someData);
+	}
+
+	//renew the page
+	function update_page(someData){
+		build_results(someData);
+		document.getElementsByClassName('pagination_page')[config.page-1].style.backgroundColor = "#81aaa7";
+		document.getElementsByClassName('pagination_page')[config.page-1 + nOfPages].style.backgroundColor = "#81aaa7"
+	}
+
+	//Building navigation bar
+	function build_nav() {
+		let page_nav = "";
+		// console.log(nOfPages)
+		for(let i = 1; i <= nOfPages; i++ ){
+			page_nav += "<li class='pagination_page'><a data-page=" + i + ">" + i + "</a></li>\n";
+		}
+		return page_nav;
+	}
+
+	function build_results(someData) {
+		document.getElementsByClassName('card_container')[0].innerHTML = '';
+		let start = ( config.page !== 1 )? (config.page - 1) * config.max_per_page : 0;
+		let end;
+		if(config.max_results - start < config.max_per_page){
+			end = config.max_results;
+		}
+		else {
+			end = config.max_per_page + start;
+		}
+
+		for(let i = 0; i < end - start; i++){
+			//
+			let parNode  = document.getElementsByClassName('card_container')[0];
+			let currNode = document.createElement('div');
+			currNode.className= "card";
+			parNode.appendChild(currNode);
+			//
+			parNode = document.getElementsByClassName('card')[i]
+			currNode = document.createElement('div');
+			currNode.className= "card_photo";
+			currNode.style.backgroundImage = `url(${someData[i + start].photos[0]})`;
+			parNode.appendChild(currNode);
+			//
+			currNode = document.createElement('div');
+			currNode.className= "card_description";
+			parNode.appendChild(currNode);
+			//
+			parNode = document.getElementsByClassName('card_photo')[i]
+			currNode = document.createElement('div');
+			currNode.className= "card_photo_cover";
+			parNode.appendChild(currNode);
+			//
+			parNode = document.getElementsByClassName('card_photo_cover')[i]
+			currNode = document.createElement('span');
+			currNode.className= "cost";
+			currNode.innerHTML = someData[i + start].cost;
+			parNode.appendChild(currNode);
+			//
+			parNode = document.getElementsByClassName('card_description')[i]
+			currNode = document.createElement('div');
+			currNode.className= "short_description text";
+			currNode.innerHTML = someData[i + start].city + ', ' + someData[i + start].district + ' district, ' + someData[i + start].address + ', ' + someData[i + start].rooms + ' rooms, ' + someData[i + start].square + ' m2';
+			parNode.appendChild(currNode);
+			//
+			parNode = document.getElementsByClassName('card_description')[i]
+			currNode = document.createElement('div');
+			currNode.className= "card_telephone text";
+			currNode.innerHTML = someData[i + start].telephone;
+			parNode.appendChild(currNode);
+		}
+	}
+	
+	window.addEventListener("load", function() {
+		axios.post("./galery.html", {})
+			.then(function(res){
+				changeConfigs(res.data);
+				init(res.data);
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	});
+
+	document.getElementsByClassName("filter_button")[0].onclick = function(){
+		// init(filteredData);
+		axios.post("./galery.html", {})
+			.then(function(res){
+				let filteredData = filter(res.data, document.getElementsByClassName('filter_city')[0].value, document.getElementsByClassName('filter_district')[0].value);
+				changeConfigs(filteredData);
+				init(filteredData);
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	}
+})();
--- test/pag.js
+++ test/pag.js
@@ -1,221 +1,206 @@
 (function(){
-	let config = {
-			max_results: 25,
-			max_per_page: 6,
-			page: 1
-		},
-		no_of_pages = Math.ceil( config.max_results / config.max_per_page ) + 1,
-		results = [];
+	let place = 'city', meaning = 'Kyiv', maxCost, minCost;
 	
-	async function request(i, start) {
-		try {
-			// let images = [];
-			const json = await axios.post('/galery.html', {
-			})
-				.then(function (response) {
-						//
-						let parNode  = document.getElementsByClassName('card_container')[0];
-						let currNode = document.createElement('div');
-						currNode.className= "card";
-						parNode.appendChild(currNode);
-						//
-						parNode = document.getElementsByClassName('card')[i]
-						currNode = document.createElement('div');
-						currNode.className= "card_photo";
-						// currNode.setAttribute('background-image', response.data[i + start].photos[0])
-						
-							axios.get(response.data[i].photos[0], {})
-								.then((response) => {
-									console.log(encodeURI(response.data));
-									document.getElementsByClassName('card_photo')[i].style.backgroundImage = `url( + ${encodeURI(response.data)} + )`;
-								});
-						parNode.appendChild(currNode);
-						//
-						currNode = document.createElement('div');
-						currNode.className= "card_description";
-						parNode.appendChild(currNode);
-						//
-						parNode = document.getElementsByClassName('card_photo')[i]
-						currNode = document.createElement('div');
-						currNode.className= "card_photo_cover";
-						parNode.appendChild(currNode);
-						//
-						parNode = document.getElementsByClassName('card_photo_cover')[i]
-						currNode = document.createElement('span');
-						currNode.className= "cost";
-						currNode.innerHTML = response.data[i + start].cost;
-						parNode.appendChild(currNode);
-						//
-						parNode = document.getElementsByClassName('card_description')[i]
-						currNode = document.createElement('div');
-						currNode.className= "short_description text";
-						currNode.innerHTML = response.data[i + start].city + ', ' + response.data[i + start].district + ' district, ' + response.data[i + start].address + ', ' + response.data[i + start].rooms + ' rooms, ' + response.data[i + start].square + ' m2';
-						parNode.appendChild(currNode);
-						//
-						parNode = document.getElementsByClassName('card_description')[i]
-						currNode = document.createElement('div');
-						currNode.className= "card_telephone text";
-						currNode.innerHTML = response.data[i + start].telephone;
-						parNode.appendChild(currNode);
-						// for( let j = 0; j < json.data[i].photos.length; j++){
-						
-				})
-				.catch(function (error) {
-					console.log(error);
-				});
-			
-			
-		} catch (e) {
-			console.log(e)
-		}
-	}
 	
-	function init(){
-		console.log('hello')
-		for( var x = 0; x < config.max_results; x++ ){
-			results[x] = "Result " + x;
-		}
-		document.getElementsByClassName("next")[0].onclick = function() {
-			pager("next");
-			return false;
-		};
-		document.getElementsByClassName("next")[1].onclick = function() {
-			pager("next");
-			return false;
-		};
-		document.getElementsByClassName("previous")[0].onclick = function() {
-			pager("previous");
-			return false;
-		};
-		document.getElementsByClassName("previous")[1].onclick = function() {
-			pager("previous");
-			return false;
-		};
-		document.getElementsByClassName("page_nav")[0].onclick = function(e) {
-			let page = e.target.getAttribute("data-page");
-			if(page){
-				pager("goto", page);
+	function filter_by_cost(value){
+		if(!maxCost || !minCost){
+			if(!maxCost && !minCost){
+				return true;
 			}
-			return false;
-		};
-		document.getElementsByClassName("page_nav")[1].onclick = function(e) {
-			let page = e.target.getAttribute("data-page");
-			if(page){
-				pager("goto", page);
+			else if(!maxCost){
+				return value.cost >= minCost;
 			}
-			return false;
-		};
-		update_page();
+			else{
+				return value.cost <= maxCost;
+			}
+		}
+		else{
+			return value.cost >= minCost && value.cost <= maxCost;
+		}
 	}
 	
 	
-	function pager(action, page) {
-		switch (action) {
-			case "next":
-				if( (config.page + 1) < no_of_pages ){
-					++config.page;
-				}
-				break;
-			
-			case "previous":
-				if( (config.page - 1) >= 1 ){
-					--config.page;
-				}
-				break;
-			
-			case "goto":
-				config.page = page;
-				break;
-			
-			default:
-				break;
+	function filter_by_place(value){
+		if(place === '' && meaning === ''){
+			return true;
 		}
-		update_page();
-	}
-	function build_nav() {
-		var i,
-			page_nav = "";
-		
-		for( i = 1; i < no_of_pages; i++ ){
-			page_nav += "<li class='pagination_page'><a data-page=" + i + ">" + i + "</a></li>\n";
+		else{
+			for(let key in value){
+				if(key === place){
+					return value[key] === meaning;
+				}
+			}
 		}
-		return page_nav;
 	}
+	
+	
+	
+	//configuration data for pagination
+	// let config = {
+	// 		max_results: 1,
+	// 		max_per_page: 1,
+	// 		page: 1
+	// 	},
+	// 	no_of_pages = 1;
+	//
+	//
+	//Function for initialisation the number of the page
+	// function init(){
+	// 	document.getElementsByClassName("next")[0].onclick = function() {
+	// 		pager("next");
+	// 		return false;
+	// 	};
+	// 	document.getElementsByClassName("next")[1].onclick = function() {
+	// 		pager("next");
+	// 		return false;
+	// 	};
+	// 	document.getElementsByClassName("previous")[0].onclick = function() {
+	// 		pager("previous");
+	// 		return false;
+	// 	};
+	// 	document.getElementsByClassName("previous")[1].onclick = function() {
+	// 		pager("previous");
+	// 		return false;
+	// 	};
+	// 	document.getElementsByClassName("page_nav")[0].onclick = function(e) {
+	// 		let page = e.target.getAttribute("data-page");
+	// 		if(page){
+	// 			pager("goto", page);
+	// 		}
+	// 		return false;
+	// 	};
+	// 	document.getElementsByClassName("page_nav")[1].onclick = function(e) {
+	// 		let page = e.target.getAttribute("data-page");
+	// 		if(page){
+	// 			pager("goto", page);
+	// 		}
+	// 		return false;
+	// 	};
+	// 	update_page();
+	// }
+	
+	// //Work with next/prev
+	// function pager(action, page) {
+	// 	switch (action) {
+	// 		case "next":
+	// 			if( (config.page + 1) < no_of_pages ){
+	// 				++config.page;
+	// 			}
+	// 			break;
+	//
+	// 		case "previous":
+	// 			if( (config.page - 1) >= 1 ){
+	// 				--config.page;
+	// 			}
+	// 			break;
+	//
+	// 		case "goto":
+	// 			config.page = page;
+	// 			break;
+	//
+	// 		default:
+	// 			break;
+	// 	}
+	// 	update_page();
+	// }
+	
+	// //Building navigation bar
+	// function build_nav() {
+	// 	let page_nav = "";
+	// 	console.log(no_of_pages)
+	// 	for(let i = 1; i <= no_of_pages; i++ ){
+	// 		page_nav += "<li class='pagination_page'><a data-page=" + i + ">" + i + "</a></li>\n";
+	// 	}
+	// 	return page_nav;
+	// }
+	
+	//Rebuilding inner page
 	function build_results(){
-		document.getElementsByClassName('pagination_page')[config.page-1].style.backgroundColor = "#4e8984"
-		document.getElementsByClassName('pagination_page')[config.page-1 + no_of_pages - 1].style.backgroundColor = "#4e8984"
 		document.getElementsByClassName('card_container')[0].innerHTML = '';
-		// var i,
-			let start = ( config.page !== 1 )? (config.page - 1) * config.max_per_page : 0;
-			let end;
-			if(config.max_results - start < config.max_per_page){
-				end = config.max_results;
-			}
-			else {
+		
+		axios.post('/galery.html', {
+		})
+			.then(function (response) {
+				
+				let data = response.data.filter(filter_by_cost);
+				data = data.filter(filter_by_place);
+				
+				config = {
+						max_results: data.length,
+						max_per_page: 6,
+						page: 1
+					}
+				no_of_pages = Math.ceil( config.max_results / config.max_per_page ) + 1
+				let start = ( config.page !== 1 )? (config.page - 1) * config.max_per_page : 0;
+				let end;
+				if(config.max_results - start < config.max_per_page){
+					end = config.max_results;
+				}
+				else {
 					end = config.max_per_page + start;
-			}
-		// 	result;
-		//
-		console.log(start + " " + end)
-		for(let i = 0; i < end - start; i++){
-			request(i, start);
-		}
-		// axios.post('/galery.html', {
-		// })
-		// 	.then(function (response) {
-		//
-		// 		for(let i = 0; i < end - start; i++){
-		// 			// console.log(i);
-		// 			let parNode  = document.getElementsByClassName('card_container')[0];
-		// 			let currNode = document.createElement('div');
-		// 			currNode.className= "card";
-		// 			parNode.appendChild(currNode);
-		// 			//
-		// 			parNode = document.getElementsByClassName('card')[i]
-		// 			currNode = document.createElement('div');
-		// 			currNode.className= "card_photo";
-		// 			currNode.setAttribute('background-image', response.data[i + start].photos[0])
-		// 			parNode.appendChild(currNode);
-		// 			//
-		// 			currNode = document.createElement('div');
-		// 			currNode.className= "card_description";
-		// 			parNode.appendChild(currNode);
-		// 			//
-		// 			parNode = document.getElementsByClassName('card_photo')[i]
-		// 			currNode = document.createElement('div');
-		// 			currNode.className= "card_photo_cover";
-		// 			parNode.appendChild(currNode);
-		// 			//
-		// 			parNode = document.getElementsByClassName('card_photo_cover')[i]
-		// 			currNode = document.createElement('span');
-		// 			currNode.className= "cost";
-		// 			currNode.innerHTML = response.data[i + start].cost;
-		// 			parNode.appendChild(currNode);
-		// 			//
-		// 			parNode = document.getElementsByClassName('card_description')[i]
-		// 			currNode = document.createElement('div');
-		// 			currNode.className= "short_description text";
-		// 			currNode.innerHTML = response.data[i + start].city + ', ' + response.data[i + start].district + ' district, ' + response.data[i + start].address + ', ' + response.data[i + start].rooms + ' rooms, ' + response.data[i + start].square + ' m2';
-		// 			parNode.appendChild(currNode);
-		// 			//
-		// 			parNode = document.getElementsByClassName('card_description')[i]
-		// 			currNode = document.createElement('div');
-		// 			currNode.className= "card_telephone text";
-		// 			currNode.innerHTML = response.data[i + start].telephone;
-		// 			parNode.appendChild(currNode);
-		// 			console.log(response);
-		// 		}
-		// 	})
-		// 	.catch(function (error) {
-		// 		console.log(error);
-		// 	});
+				}
+				
+				
+				for(let i = 0; i < end - start; i++){
+					//
+					let parNode  = document.getElementsByClassName('card_container')[0];
+					let currNode = document.createElement('div');
+					currNode.className= "card";
+					parNode.appendChild(currNode);
+					//
+					parNode = document.getElementsByClassName('card')[i]
+					currNode = document.createElement('div');
+					currNode.className= "card_photo";
+					currNode.style.backgroundImage = `url(${data[i + start].photos[0]})`;
+					parNode.appendChild(currNode);
+					//
+					currNode = document.createElement('div');
+					currNode.className= "card_description";
+					parNode.appendChild(currNode);
+					//
+					parNode = document.getElementsByClassName('card_photo')[i]
+					currNode = document.createElement('div');
+					currNode.className= "card_photo_cover";
+					parNode.appendChild(currNode);
+					//
+					parNode = document.getElementsByClassName('card_photo_cover')[i]
+					currNode = document.createElement('span');
+					currNode.className= "cost";
+					currNode.innerHTML = data[i + start].cost;
+					parNode.appendChild(currNode);
+					//
+					parNode = document.getElementsByClassName('card_description')[i]
+					currNode = document.createElement('div');
+					currNode.className= "short_description text";
+					currNode.innerHTML = data[i + start].city + ', ' + response.data[i + start].district + ' district, ' + response.data[i + start].address + ', ' + response.data[i + start].rooms + ' rooms, ' + response.data[i + start].square + ' m2';
+					parNode.appendChild(currNode);
+					//
+					parNode = document.getElementsByClassName('card_description')[i]
+					currNode = document.createElement('div');
+					currNode.className= "card_telephone text";
+					currNode.innerHTML = data[i + start].telephone;
+					parNode.appendChild(currNode);
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
 	}
-	function update_page(){
-		document.getElementsByClassName("page_nav")[0].innerHTML = build_nav();
-		document.getElementsByClassName("page_nav")[1].innerHTML = build_nav();
-		build_results()
-	}
+	
+	//renew the page
+	// async function update_page(){
+	// 	document.getElementsByClassName("page_nav")[0].innerHTML = build_nav();
+	// 	document.getElementsByClassName("page_nav")[1].innerHTML = build_nav();
+	// 	await build_results();
+	// 	// document.getElementsByClassName('pagination_page')[config.page-1].style.backgroundColor = "#81aaa7";
+	// 	// document.getElementsByClassName('pagination_page')[config.page-1 + no_of_pages - 1].style.backgroundColor = "#81aaa7"
+	// }
+	
 	window.addEventListener("load", function() {
 		init();
 	});
+	
+	document.getElementsByClassName("filter_button")[0].onclick = function(){
+		init()
+	}
 })();
